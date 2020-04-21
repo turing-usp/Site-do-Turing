04fa369a5952cb04f79ae45161d809242491b41e
William Fukushima <wil.fukushima@gmail.com>
2020-04-21 16:00:01 -0300

    Rodapé Das páginas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Cabecalho from './Cabecalho'
-import Faq from './Faq'
+import Cabecalho from './Cabecalho';
+import Rodape from './Rodape';
+import Faq from './Faq';
 
 ReactDOM.render(<div>
     <Cabecalho />
     <Faq />
+    <Rodape />
     </div>, 
     document.getElementById('root')
     )
