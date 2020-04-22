72231b7719cf025c94067fa8b764e69395e52a42
William Fukushima <wil.fukushima@gmail.com>
2020-04-22 07:44:29 -0300

    Paginação e debug

--- frontend/src/pages/Events/index.js
+++ frontend/src/pages/Events/index.js
@@ -10,6 +10,17 @@ export default function Events () {
     return (
         <div>
             <Cabecalho />
+            <h1>Nossos Eventos</h1>
+            <p class='description'>
+                Sasuke Uchiha (うちはサスケ, Uchiha Sasuke) é um dos últimos membros sobreviventes do clã Uchiha de Konohagakure, além de ser a reencarnação atual de Indra. Ele se tornou um shinobi para que pudesse algum dia ficar forte o suficiente para se vingar contra o seu irmão mais velho, Itachi, que tinha massacrado todo o seu clã. Inicialmente, um membro do Time Kakashi de Konoha...
+            </p>
+            <h1>Próximos Eventos</h1>
+            <h1>Eventos Realizados</h1>
+            <div class="wrapper">
+                <button class='more_events_box'>
+                    Mais Eventos
+                </button>
+            </div>
             <Rodape />
         </div>
     );
