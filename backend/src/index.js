cb1853d2013fe190484a6e2ed9c9fcafcad82ea9
williamfukushima <51289763+williamfukushima@users.noreply.github.com>
2020-04-27 10:42:22 -0300

    Projects (#25)
    * feat: frontend: { projects page + backend integration + assets change} backend: CORS module addition
    
    * feat:frontend{tag search}backend{Controller query}
    
    * Página Projetos (#24)
    
    approved-by: @leonardomurakami

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-
-app.use(routes)
+app.use(cors());
+app.use(routes);
 
 app.listen(3333); //utilize route 3333 for backend on development
 
--- frontend/src/Tags/CabecalhoGhost.js
+++ frontend/src/Tags/CabecalhoGhost.js
@@ -6,12 +6,6 @@ import logo from '../assets/logo.png';
 function CabecalhoGhost () {
     return (
         <div class= "rect_cabecalho_ghost">
-            <img class = "cabecalho_logo" src={logo} alt="turing" />
-            <span class="text_cabecalho">Quem somos</span>
-            <span class="text_cabecalho">Eventos</span> 
-            <span class="text_cabecalho">Projetos</span>
-            <span class="text_cabecalho">Aprenda IA</span>
-            <span class="text_cabecalho">Fale Conosco</span>
         </div>
     );
 }
