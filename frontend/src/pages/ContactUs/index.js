60f54ed5a4277b3acd9105a62c79277515f83507
Gbecdox <41247052+Gbecdox@users.noreply.github.com>
2020-05-02 06:31:06 -0300

    Formulário de contato atualizado (#29)
    Foi adicionado:
    
    - Ícones a cada campo de entrada;
    - Sistema de validação do formulário;
    - Mensagem de erro ao tentar submeter um formulário inválido;
    - Feedback visual ao enviar um formulário válido.

--- frontend/src/pages/ContactUs/index.js
+++ frontend/src/pages/ContactUs/index.js
@@ -1,38 +1,112 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from 'react-dom';
 import logo from '../../assets/logo.png';
 import './styles.css';
 import {FiPlus} from 'react-icons/fi'
+import { BsChatQuoteFill, BsFillPersonFill } from 'react-icons/bs'
+import {MdEmail} from 'react-icons/md'
+import {FaSuitcase} from 'react-icons/fa'
+import {AiFillFileText} from 'react-icons/ai'
+import check from '../../assets/check.gif'
 import Cabecalho from '../../Tags/Cabecalho.js'
 import Rodape from '../../Tags/Rodape.js'
 
+
 export default function ContactUs () {
-  
-  
+
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
     const [grupo, setGrupo] = useState('')
     const [assunto, setAssunto] = useState('')
     const [mensagem, setMensagem] = useState('')
 
-    function onSubmit(e) {
+    const [errorNome, setErrorNome] = useState('')
+    const [errorEmail, setErrorEmail] = useState('')
+    const [errorAssunto, setErrorAssunto] = useState('')
+    const [errorMensagem, setErrorMensagem] = useState('')
+
+    const [submited, setSubmited] = useState(false)
+    const [validated, setValidated] = useState(false)
+
+    useEffect (() => {
+        if (submited &&
+            !errorNome &&
+            !errorEmail &&
+            !errorAssunto &&
+            !errorMensagem) {
+
+            setValidated(true)
+
+            const data = {
+                nome,
+                email,
+                grupo,
+                assunto,
+                mensagem
+            };
+
+            setNome('')
+            setEmail('')
+            setGrupo('')
+            setAssunto('')
+            setMensagem('')
+
+            setTimeout(() => {
+                setValidated(false)
+            }, 3000)
+            
+            setSubmited(false)
+        }
+    }, [submited, errorNome, errorEmail, errorAssunto, errorMensagem])
+
+    function validateNome(nome) {
+        if (!nome) {
+            setErrorNome('Campo obrigatório.')
+        }
+        else {
+            setErrorNome('')
+        }
+    }
+
+    function validateEmail(email) {
+        if (!email) {
+            setErrorEmail('Campo obrigatório.')
+        }
+        else if (!/\S+@\S+\.\S+/.test(email)) {
+            setErrorEmail('E-mail inválido.')
+        }
+        else {
+            setErrorEmail('')
+        }
+    }
+
+    function validateAssunto(assunto) {
+        if (!assunto) {
+            setErrorAssunto('Campo obrigatório.')
+        }
+        else {
+            setErrorAssunto('')
+        }
+    }
+
+    function validateMensagem(mensagem) {
+        if (!mensagem) {
+            setErrorMensagem('Campo obrigatório.')
+        }
+        else {
+            setErrorMensagem('')
+        }
+    }
+
+    function handleSubmit(e) {
         e.preventDefault()
 
-        const data = {
-            nome,
-            email,
-            grupo,
-            assunto,
-            mensagem,
-        };        
-
-        console.log(data); /** Alterar depois */
-
-        setNome('')
-        setEmail('')
-        setGrupo('')
-        setAssunto('')
-        setMensagem('')
+        setSubmited(true)
+ 
+        validateNome(nome)
+        validateEmail(email)
+        validateAssunto(assunto)
+        validateMensagem(mensagem)
     }
 
     const [numeroFaq, setNumero] = useState(2);
@@ -44,44 +118,63 @@ export default function ContactUs () {
 
     return (
         <div class='container'>
-            <Cabecalho />    
+            <Cabecalho />
 
+            <span class="title_form">Contato</span>
             <form class='formulario'>
-                <input
-                    class='entrada'
-                    value={nome}
-                    onChange={e => setNome(e.target.value)} 
-                    placeholder='Nome'
-                />
-                <br />
-                <input
-                    class = 'entrada'
-                    value={email} 
-                    onChange={e => setEmail(e.target.value)} 
-                    placeholder='E-mail'
-                />
-                <br />
-                <input
-                    class = 'entrada'
-                    value={grupo} 
-                    onChange={e => setGrupo(e.target.value)} 
-                    placeholder='Grupo/Empresa'
-                />
-                <br />
-                <input
-                    class='entrada'
-                    value={assunto} 
-                    onChange={e => setAssunto(e.target.value)} 
-                    placeholder='Assunto'
-                />
-                <br />
-                <textarea
-                    class='mensagem'
-                    value={mensagem} 
-                    onChange={e => setMensagem(e.target.value)} 
-                    placeholder='Escreva sua mensagem'
-                />
-                <button class='enviar' onClick={onSubmit}><span>Enviar</span></button>
+                <div class='container_entrada'>
+                    <BsFillPersonFill color='rgb(100, 100, 100)' size='1.3em'/>
+                    <input
+                        class={errorNome ? 'entrada_error':'entrada'}
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                        placeholder='Nome'
+                    />
+                    {errorNome && <p class='text_error_entrada'>{errorNome}</p>}
+                </div>
+                <div class='container_entrada'>
+                    <MdEmail color='rgb(100, 100, 100)' size='1.3em'/>
+                    <input
+                        class={errorEmail ? 'entrada_error':'entrada'}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder='E-mail'
+                    />
+                    {errorEmail && <p class='text_error_entrada'>{errorEmail}</p>}
+                </div>
+                <div class='container_entrada'>
+                    <FaSuitcase color='rgb(100, 100, 100)' size='1.3em'/>
+                    <input
+                        class='entrada'
+                        value={grupo}
+                        onChange={e => setGrupo(e.target.value)}
+                        placeholder='Grupo/Empresa'
+                    />
+                </div>
+                <div class='container_entrada'>
+                    <BsChatQuoteFill color='rgb(100, 100, 100)' size='1.3em'/>
+                    <input
+                        class={errorAssunto ? 'entrada_error':'entrada'}
+                        value={assunto}
+                        onChange={e => setAssunto(e.target.value)}
+                        placeholder='Assunto'
+                    />
+                    {errorAssunto && <p class='text_error_entrada'>{errorAssunto}</p>}
+                </div>
+                <div class='container_mensagem'>
+                    <AiFillFileText color='rgb(100, 100, 100)' size='1.3em'/>
+                    <textarea
+                        class={errorMensagem ? 'mensagem_error':'mensagem'}
+                        value={mensagem}
+                        onChange={e => setMensagem(e.target.value)}
+                        placeholder='Escreva sua mensagem'
+                    />                                   
+                    {!validated && <button class='enviar' onClick={handleSubmit}><span>Enviar</span></button>}
+                    {validated && <img class='check' src={check} />}
+                    <div class='container_text_error_mensagem'>
+                        {errorMensagem && <p class='text_error_mensagem'>{errorMensagem}</p>}
+                    </div>
+                </div>
             </form>
 
             <div class="faq_and_answer">
