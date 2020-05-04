bf1652885864d0c10c120c80ebf3ee1abe63c4b1
Leonardo Murakami <51211530+leonardomurakami@users.noreply.github.com>
2020-05-03 22:04:31 -0300

    fix/configjs dummy (#39)
    * create dummy config.js
    
    * Update routes.js
    
    * Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,12 +7,12 @@ const mediumController = require('./controllers/mediumController');
 const faqController = require('./controllers/faqController');
 const eventsController = require('./controllers/eventsController');
 const projectsController = require('./controllers/projectsController');
-//const formsController = require('./controllers/formsController.js')
+const formsController = require('./controllers/formsController.js')
 
 routes.get('/members', membersController.index);
 routes.post('/members', membersController.create);
-//routes.get('/sendContact', formsController.index);
-//routes.post('/sendContact', formsController.create);
+routes.get('/sendContact', formsController.index);
+routes.post('/sendContact', formsController.create);
 routes.get('/listMediumPosts', mediumController.index);
 routes.post('/faq', faqController.create);
 routes.get('/faq', faqController.index);
