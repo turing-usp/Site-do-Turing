9ba49bfc6f4d86091d93683649537271807c36f1
= <luisaheise3277@gmail.com>
2020-05-10 21:10:17 -0300

    membros e responsividade no footer

--- frontend/src/pages/AboutUs/index.js
+++ frontend/src/pages/AboutUs/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import logo from '../../assets/logo.png';
+import github from '../../assets/github_icon.png';
+import {FiMail} from 'react-icons/fi';
+import membro_padrao from "../../assets/avatar_placeholder.png";
 import './styles.css';
 import {FiPlus} from 'react-icons/fi'
 import Cabecalho from '../../Tags/Cabecalho.js'
@@ -41,6 +44,99 @@ export default function AboutUs () {
                 e a habilidade de trabalhar em equipe tomando atitudes,
                 mas mantendo-se aberto a sugestões.
             </p>
+            <br/>
+            <h1 class='titulo_aboutus'>Nossos Membros</h1>
+            <h2 class="tipos_de_membros">Diretores</h2>
+
+            <div class="container_membros">
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Felipe Azank</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Lucas Sepeda</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="tipos_de_membros">Membros</h2>
+
+            <div class="container_membros">
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Ciclano</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Fulano</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h2 class="tipos_de_membros">Trainees</h2>
+            <div class="container_membros">
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">X Æ A-12</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Inês Brasil</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container_membro">
+                    <div class="cartao">
+                        <span class="nome_membro">Gretchen</span>
+                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
+                        <div class="contato_membro">
+                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
+                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
             <Rodape />
         </div>
     );
