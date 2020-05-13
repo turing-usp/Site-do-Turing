788fbef9f6650ed26475e8a5133195a4e6f945ae
Leonardo Murakami <51211530+leonardomurakami@users.noreply.github.com>
2020-05-13 15:57:03 -0300

    Adiciona variaveis para produção (#55)
    * Adiciona variaveis para produção
    
    * Conserta dependencias do frontend

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,5 +1,4 @@
 // Update with your config settings.
-
 module.exports = {
 
   development: {
@@ -16,9 +15,9 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
     },
     pool: {
       min: 2,
@@ -32,9 +31,9 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL,
+      user:     process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
     },
     pool: {
       min: 2,
--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 let secrets = {
-    MAILJET_PUBLIC_API_KEY: "secret",
-    MAILJET_SECRET_API_KEY: "secret"
+    MAILJET_PUBLIC_API_KEY: process.env.MAILJET_PUBLIC_API_KEY || "secret",
+    MAILJET_SECRET_API_KEY: process.env.MAILJET_SECRET_API_KEY || "secret"
 };
 
 module.exports = { secrets }
