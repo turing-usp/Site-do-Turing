1edbf64bd2e5bf4d4fa61f85b33c16907010c2a3
Luísa Mendes Heise <43735789+luisaheise@users.noreply.github.com>
2020-05-16 13:26:48 -0300

    membros arrumado (#58)
    * membros arrumado
    
    * Conserta hard conflicts
    
    * Atualiza AboutUs/index.js para resolver conflitos
    
    Co-authored-by: Leonardo Murakami <leonardo.murakami@loft.com.br>
    Co-authored-by: Leonardo Murakami <51211530+leonardomurakami@users.noreply.github.com>

--- frontend/src/pages/AboutUs/index.js
+++ frontend/src/pages/AboutUs/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import logo from '../../assets/logo.png';
 import github from '../../assets/github_icon.png';
+import linkedin from '../../assets/in.png';
 import {FiMail} from 'react-icons/fi';
 import membro_padrao from "../../assets/avatar_placeholder.png";
 import './styles.css';
@@ -11,7 +12,26 @@ import Rodape from '../../Tags/Rodape.js'
 
 import api from '../../services/api.js'
 
-export default function AboutUs () {
+export default function AboutUs () { 
+    
+    const [diretores, setDiretores] = useState([]);
+    const [membros, setMembros] = useState([]);
+    const [trainees, setTrainees] = useState([]);
+
+    useEffect(() => {
+        api.get(`members?role=dir`).then(response => {
+            setDiretores(response.data);
+        });
+
+        api.get(`members?role=memb`).then(response => {
+            setMembros(response.data);
+        });
+
+        api.get(`members?role=train`).then(response => {
+            setTrainees(response.data);
+        });
+        }, []);
+    
     return (
         <div class='container'>
             <Cabecalho />
@@ -47,98 +67,87 @@ export default function AboutUs () {
                 mas mantendo-se aberto a sugestões.
             </p>
             <br/>
+
             <h1 class='titulo_aboutus'>Nossos Membros</h1>
             <h2 class="tipos_de_membros">Diretores</h2>
 
+
             <div class="container_membros">
+            {diretores.map(diretor => (
                 <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Felipe Azank</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                <div class="cartao">
+                    <span class="nome_membro">{diretor.name}</span>
+                    <div class="foto_membro"><img src={diretor.photoLink} alt="Membro"/></div>
+                    <div class="contato_membro">
+                        <div class="email_membro">
+                            <a href={"mailto:"+ diretor.email}>
+                                    <FiMail size={10} color="#F2994A"/> 
+                            </a>
+                            {diretor.email}
                         </div>
-                    </div>
-                </div>
-
-
-                <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Lucas Sepeda</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        <div class="rede_membro">
+                            <a target="_blank" href={diretor.github}><div class="github_membro"><img src={github} alt="github"/></div></a>
+                            <a target="_blank" href={diretor.linkedin}><div class="linkedin_membro"><img src={linkedin} alt="github"/></div></a>
                         </div>
                     </div>
                 </div>
             </div>
+            ))}
+            </div>
+            
+            
 
             <h2 class="tipos_de_membros">Membros</h2>
 
             <div class="container_membros">
+            {membros.map(membro => (
                 <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Ciclano</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                <div class="cartao">
+                    <span class="nome_membro">{membro.name}</span>
+                    <div class="foto_membro"><img src={membro.photoLink} alt="Membro"/></div>
+                    <div class="contato_membro">
+                        <div class="email_membro">
+                            <a href={"mailto:"+ membro.email}>
+                                    <FiMail size={10} color="#F2994A"/> 
+                            </a>
+                            {membro.email}
                         </div>
-                    </div>
-                </div>
-
-
-                <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Fulano</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        <div class="rede_membro">
+                            <a target="_blank" href={membro.github}><div class="github_membro"><img src={github} alt="github"/></div></a>
+                            <a target="_blank" href={membro.linkedin}><div class="linkedin_membro"><img src={linkedin} alt="github"/></div></a>
                         </div>
                     </div>
                 </div>
             </div>
+            ))}
+            </div>
+
+          
 
             <h2 class="tipos_de_membros">Trainees</h2>
+
             <div class="container_membros">
+            {trainees.map(trainee => (
                 <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">X Æ A-12</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                <div class="cartao">
+                    <span class="nome_membro">{trainee.name}</span>
+                    <div class="foto_membro"><img src={trainee.photoLink} alt="Membro"/></div>
+                    <div class="contato_membro">
+                        <div class="email_membro">
+                            <a href={"mailto:"+ trainee.email}>
+                                    <FiMail size={10} color="#F2994A"/> 
+                            </a>
+                            {trainee.email}
                         </div>
-                    </div>
-                </div>
-
-
-                <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Inês Brasil</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container_membro">
-                    <div class="cartao">
-                        <span class="nome_membro">Gretchen</span>
-                        <div class="foto_membro"><img src={membro_padrao} alt="Membro"/></div>
-                        <div class="contato_membro">
-                            <a href="mailto:azank@azank.com" class="email_membro"><FiMail size={20} color="#F2994A"/></a>
-                            <a href="www.gitdoazank.com"><div class="github_membro"><img src={github} alt="github"/></div></a>
+                        <div class="rede_membro">
+                            <a target="_blank" href={trainee.github}><div class="github_membro"><img src={github} alt="github"/></div></a>
+                            <a target="_blank" href={trainee.linkedin}><div class="linkedin_membro"><img src={linkedin} alt="github"/></div></a>
                         </div>
                     </div>
                 </div>
             </div>
-            
+            ))}
+            </div>
             <Rodape />
         </div>
     );
