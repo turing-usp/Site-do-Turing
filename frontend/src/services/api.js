bc410b709bf0fe1645eb89681a2a1672bef4e87d
Leonardo Murakami <51211530+leonardomurakami@users.noreply.github.com>
2020-07-29 20:45:00 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://sitegrupoturing.herokuapp.com/"
+    baseURL: 'https://sitegrupoturing.herokuapp.com/'
 });
 
 export default api;
