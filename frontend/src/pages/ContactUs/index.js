6e0c2fc874ddaba4cca76a7be7c7dee5120e8bdd
Gabriel Mossato <41247052+gvmossato@users.noreply.github.com>
2020-08-19 09:24:53 -0300

    ContactUs: novo formulário e alterações no faq (#98)
    FAQ:
    - Renomeação de classes
    - Container de perguntas com backgroud visível
    - Novos ícones para interação com as perguntas
    
    FORMULÁRIO:
    - Redução da quantidade de estados
    - Redução do número de funções
    - Renomeação de classes
    - Correção de desalinhamentos e reposicionamento de elementos
    - Customização dos ícones
    - Novo botão de envio: maior feedback ao usuário, descarta uso de assets externas
    - Responsividade
    
    ALTERADA A ORDEM DO FAQ E DO FORMULÁRIO NA ESTRUTURA DA PÁGINA

--- frontend/src/pages/ContactUs/index.js
+++ frontend/src/pages/ContactUs/index.js
@@ -1,180 +1,156 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from 'react-dom';
-import logo from '../../assets/logo.png';
+
 import './styles.css';
-import {FiPlus} from 'react-icons/fi'
-import { BsChatQuoteFill, BsFillPersonFill } from 'react-icons/bs'
-import {MdEmail} from 'react-icons/md'
-import {FaSuitcase} from 'react-icons/fa'
-import {AiFillFileText} from 'react-icons/ai'
-import check from '../../assets/check.gif'
+
+import { RiArrowDownSLine, RiCloseLine } from 'react-icons/ri'
+import { BsPersonFill, BsBriefcaseFill } from 'react-icons/bs'
+import { AiFillMessage, AiFillFileText } from 'react-icons/ai'
+import { MdEmail } from 'react-icons/md'
+
 import Cabecalho from '../../Tags/Cabecalho.js'
 import Rodape from '../../Tags/Rodape.js'
-import plussign from "../../assets/plus.svg"
-
 
 import api from '../../services/api.js'
 
 
 export default function ContactUs () {
 
-
-    /* Estados */
-
-
-
-
-    /* Estados Forms */
-    const [nome, setNome] = useState('')
-    const [email, setEmail] = useState('')
-    const [grupo, setGrupo] = useState('')
-    const [assunto, setAssunto] = useState('')
-    const [mensagem, setMensagem] = useState('')
-
-    const [errorNome, setErrorNome] = useState('')
-    const [errorEmail, setErrorEmail] = useState('')
-    const [errorAssunto, setErrorAssunto] = useState('')
-    const [errorMensagem, setErrorMensagem] = useState('')
-
-    const [submited, setSubmited] = useState(false)
-    const [validated, setValidated] = useState(false)
-
+    /* ESTADOS */
 
     /* Estados Faqs */
 
+    const [faqs, setFaqs] = useState([]);
+    const [questionNumber, setQuestionNumber] = useState(null);
 
-    const [faqs, setFaqs] = useState([])
-    const [questionNumber, setQuestionNumber] = useState(null)
-  
+    /* Estados Forms */
 
+    const [inputs, updateInputs] = useState({
+        'name' : '',
+        'mail' : '',
+        'motivation' : '',
+        'entity' : '',
+        'message' : ''
+    });
+    const [errors, updateErrors] = useState({
+        'name' : '',
+        'mail' : '',
+        'motivation' : '',
+        'message' : ''
+    });
+    const [isValid, setValid] = useState(false);
+    const [btnClass, setBtnClass] = useState('static')
 
-        /** Integrações **/
-
-
+    /** INTEGRAÇÕES **/
 
     useEffect (() => {
 
-
-
-        /** Integração forms **/
-
-
-
-        if (submited &&
-            !errorNome &&
-            !errorEmail &&
-            !errorAssunto &&
-            !errorMensagem) {
-
-            setValidated(true)
-            
-            const data = {
-                "contactName": nome,
-                "email":email,
-                "entity":grupo,
-                "message" :mensagem,
-                "motivation" : assunto
-            };
-    
-            api.post('/sendContact', data);
-    
-            setNome('')
-            setEmail('')
-            setGrupo('')
-            setAssunto('')
-            setMensagem('')
-
-            setTimeout(() => {
-                setValidated(false)
-            }, 3000)
-            
-            setSubmited(false)
-        };
-
-
-
         /** Integração Faqs **/
-
 
         api.get('/faq').then(response => {
             setFaqs(response.data);
         });
+    }, [])
 
+    useEffect (() => {
 
-    }, [submited, errorNome, errorEmail, errorAssunto, errorMensagem])
+        /** Integração forms **/
 
+        if (isValid) {
 
+            const toBackend = {
+                "contactName" : inputs.name,
+                "mail" : inputs.mail,
+                "motivation" : inputs.motivation,
+                "entity" : inputs.entity,
+                "message" : inputs.message,
+            };
+            api.post('/sendContact', toBackend);
+    
+            updateInputs({
+                'name' : '',
+                'mail' : '',
+                'motivation' : '',
+                'entity' : '',
+                'message' : ''
+            });
 
-    /*** Funções ***/
+            setValid(false)
+        };
+    }, [isValid]);
 
+    /*** FUNÇÕES ***/
 
+    /*** Funções Faqs ***/
 
-
+    function showAnswer(index) {
+        setQuestionNumber(questionNumber === index ? null : index);
+    }    
 
     /***  Funções Forms ***/
-
-
-
-    function validateNome(nome) {
-        if (!nome) {
-            setErrorNome('Campo obrigatório.')
-        }
-        else {
-            setErrorNome('')
-        }
-    }
-
-    function validateEmail(email) {
-        if (!email) {
-            setErrorEmail('Campo obrigatório.')
-        }
-        else if (!/\S+@\S+\.\S+/.test(email)) {
-            setErrorEmail('E-mail inválido.')
-        }
-        else {
-            setErrorEmail('')
-        }
-    }
-
-    function validateAssunto(assunto) {
-        if (!assunto) {
-            setErrorAssunto('Campo obrigatório.')
-        }
-        else {
-            setErrorAssunto('')
-        }
-    }
-
-    function validateMensagem(mensagem) {
-        if (!mensagem) {
-            setErrorMensagem('Campo obrigatório.')
-        }
-        else {
-            setErrorMensagem('')
-        }
-    }
 
     function handleSubmit(e) {
         e.preventDefault()
 
-        setSubmited(true)
- 
-        validateNome(nome)
-        validateEmail(email)
-        validateAssunto(assunto)
-        validateMensagem(mensagem)
+        for (const key of Object.keys(errors)) {
+            if (!inputs[key]) {
+                errors[key] = 'Campo obrigatório'
+            }
+            else {
+                errors[key] = ''
+            }
+        }
+
+        if (inputs['mail'] && !/\S+@\S+\.\S+/.test(inputs['mail'])) {
+            errors['mail'] = 'E-mail inválido'
+        }
+
+        updateErrors({...errors})
+
+        for (const error of Object.values(errors)) {
+            if (error) {
+                animateBtn('failure')
+                return
+            }
+        }
+
+        setValid(true)
+        animateBtn('success')
     }
 
+    function animateBtn(outcome) {
 
+        if (btnClass.includes('moving')) {
+            return
+        }
+        
+        if (outcome === 'success') {
+            setBtnClass('moving round');   
 
-    /*** Funções Faqs ***/
+            setTimeout(() => {
+                setBtnClass('moving round loading')
+            }, 125);
+            setTimeout(() => {
+                setBtnClass('moving success pulsate')
+            }, 1500);
+            setTimeout(() => {
+                setBtnClass('static')
+            }, 3300);
+        }
+        else {
+            setBtnClass('moving round fail pulsate'); 
 
-
-
-    function showAnswer(index) {
-      setQuestionNumber(questionNumber === index ? null : index);
+            setTimeout(() => {
+                setBtnClass('static');
+            }, 1500);
+        }
     }
 
+    const updateField = e => {
+        updateInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
 
     /************/
     /*          */
@@ -182,92 +158,101 @@ export default function ContactUs () {
     /*          */
     /************/
 
-
     return (
-        <div class='container'>
+        <div class='container'> 
             <Cabecalho />
 
-
-            {/* Forms */}
-
-
-            <div class="title_form">Contato</div>
-            <form class='formulario'>
-                <div class='container_entrada'>
-                    <BsFillPersonFill color='rgb(100, 100, 100)' size='1.3em' class='icon'/>
-                    
-                    <input
-                        class={errorNome ? 'entrada_error':'entrada'}
-                        value={nome}
-                        onChange={e => setNome(e.target.value)}
-                        placeholder='Nome'
-                    />
-                    {errorNome && <p class='text_error_entrada'>{errorNome}</p>}
-                </div>
-                <div class='container_entrada'>
-                    <MdEmail color='rgb(100, 100, 100)' size='1.3em' class='icon'/>
-                    <input
-                        class={errorEmail ? 'entrada_error':'entrada'}
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        placeholder='E-mail'
-                    />
-                    {errorEmail && <p class='text_error_entrada'>{errorEmail}</p>}
-                </div>
-                <div class='container_entrada'>
-                    <FaSuitcase color='rgb(100, 100, 100)' size='1.3em' class='icon'/>
-                    <input
-                        class='entrada'
-                        value={grupo}
-                        onChange={e => setGrupo(e.target.value)}
-                        placeholder='Grupo/Empresa'
-                    />
-                </div>
-                <div class='container_entrada'>
-                    <BsChatQuoteFill color='rgb(100, 100, 100)' size='1.3em' class='icon'/>
-                    <input
-                        class={errorAssunto ? 'entrada_error':'entrada'}
-                        value={assunto}
-                        onChange={e => setAssunto(e.target.value)}
-                        placeholder='Assunto'
-                    />
-                    {errorAssunto && <p class='text_error_entrada'>{errorAssunto}</p>}
-                </div>
-                <div class='container_mensagem'>
-                    <AiFillFileText color='rgb(100, 100, 100)' size='1.3em' class='icon'/>
-                    <textarea
-                        class={errorMensagem ? 'mensagem_error':'mensagem'}
-                        value={mensagem}
-                        onChange={e => setMensagem(e.target.value)}
-                        placeholder='Escreva sua mensagem'
-                    />                                   
-                    {!validated && <button class='btn_enviar' onClick={handleSubmit}><span class="span_enviar">Enviar</span></button>}
-                    {validated && <img class='check' src={check} />}
-                    {errorMensagem && <p class='text_error_mensagem'>{errorMensagem}</p>}
-                </div>
-            </form>
-
-
-            {/* Faqs */}
-
-
-
-            <div class="faq_and_answer">
-                <span class="title_faqs">FAQs</span>
-                <div class= "faq_box">
+            <div class="faq">
+                <span class="title">FAQs</span>
                 {faqs.map(({question, answer}, index) => (
-                    <div>
-                    <div class='question_and_btn'>
-                        <p class='question' onClick={() => showAnswer(index)}>{question}</p>
-                        <button class="btnAnswer" onClick={() => showAnswer(index)}>
-                            <img class="button_faq" src={plussign}/>
-                        </button>
-                    </div>
-                    <p class={`answer${questionNumber === index ? '_show' : '_hidden'}`}>{answer}</p>
+                    <div class='qtn_ans_ctr'>
+                        <div class='question_ctr'>
+                            <p class='question' onClick={() => showAnswer(index)}>{question}</p>
+                            <button class="toggler" onClick={() => showAnswer(index)}>
+                                {questionNumber === index ? <RiCloseLine class='toggler_icon' /> : <RiArrowDownSLine class='toggler_icon' />}
+                            </button>
+                        </div>
+                        <p class={`answer ${questionNumber === index ? '' : 'hide'}`}>{answer}</p>
                     </div>
                 ))}
-                </div>
             </div>
+
+            <div class='forms'>    
+                <span class='title'>Contato</span>
+                <form>
+                    <div class='field_ctr'>
+                        <div class='icon_bkgd'>
+                            <BsPersonFill class='icon' />
+                        </div>
+                        <input
+                        name='name'
+                        class={`field ${errors['name'] ? 'error' : ''}`}
+                        value={inputs.name}
+                        onChange={updateField}
+                        placeholder='Nome *' />
+                        {errors['name'] && <div class='txt_ctr'>
+                            <p class='txt_error'>{errors['name']}</p>
+                        </div>}
+                    </div>
+                    <div class='field_ctr'>
+                        <div class='icon_bkgd'>
+                            <MdEmail class='icon' />
+                        </div>
+                        <input
+                        name='mail'
+                        class={`field ${errors['mail'] ? 'error' : ''}`}
+                        value={inputs.mail}
+                        onChange={updateField}
+                        placeholder='E-mail *' />
+                        {errors['mail'] && <div class='txt_ctr'>
+                            <p class='txt_error'>{errors['mail']}</p>
+                        </div>}             
+                    </div>
+                    <div class='field_ctr'>
+                        <div class='icon_bkgd'>
+                            <AiFillMessage class='icon' />
+                        </div>
+                        <input
+                        name='motivation'
+                        class={`field ${errors['motivation'] ? 'error' : ''}`}
+                        value={inputs.motivation}
+                        onChange={updateField}
+                        placeholder='Assunto *' />
+                        {errors['motivation'] && <div class='txt_ctr'>
+                            <p class='txt_error'>{errors['motivation']}</p>
+                        </div>}
+                    </div>
+                    <div class='field_ctr'>
+                        <div class='icon_bkgd'>
+                            <BsBriefcaseFill class='icon' />
+                        </div>
+                        <input
+                        name='entity'
+                        class='field'
+                        value={inputs.entity}
+                        onChange={updateField}
+                        placeholder='Grupo/Empresa' />
+                    </div>
+                    <div class='field_ctr'>
+                        <div class='icon_bkgd'>
+                            <AiFillFileText class='icon' />
+                        </div>
+                        <textarea
+                        name='message'
+                        class={`msg_field ${errors['message'] ? 'error' : ''}`}
+                        value={inputs.message}
+                        onChange={updateField}
+                        placeholder='Escreva sua mensagem *' />                        
+                        {errors['message'] && <div class='txt_ctr'>
+                            <p class='txt_error'>{errors['message']}</p>
+                        </div>}                       
+                        <div class='submit_ctr'>
+                            <button class={btnClass} onClick={handleSubmit} />
+                        </div>
+                    </div>
+                </form>
+            </div>
+
             <Rodape />
         </div>
     );
