39034beebae02f48e2cc6c696406e7f7105393ba
Gabriel Mossato <41247052+gvmossato@users.noreply.github.com>
2020-08-24 13:02:47 -0300

    Grid de eventos análogo ao de posts do medium

--- frontend/src/pages/Events/index.js
+++ frontend/src/pages/Events/index.js
@@ -1,80 +1,91 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM, { render } from 'react-dom';
-import logo from '../../assets/logo.png';
+
 import './styles.css';
-import {FiPlus} from 'react-icons/fi'
+
 import Cabecalho from '../../Tags/Cabecalho.js'
 import Rodape from '../../Tags/Rodape.js'
 
 import api from '../../services/api.js'
 
 export default function Events () {
-    const [eventosNaoRealizados, setEventosNaoRealizados] = useState([]);
-    const [eventosRealizados, setEventosRealizados] = useState([]);
-    const [showMoreEvents, setShowMoreEvent] = useState(false);
-    const [showButton, setShowButton] = useState(true);
 
+    const [nextEvents, setNextEvents] = useState([]);
+    const [doneEvents, setDoneEvents] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
         api.get(`events?realizado=nao`).then(response => {
-            setEventosNaoRealizados(response.data);
+            setNextEvents(response.data);
         });
 
-        if(showMoreEvents){
-            api.get(`events?realizado=sim`).then(response => {
-                setEventosRealizados(response.data);
-            });
-        }
+        api.get(`events?realizado=sim`).then(response => {
+            setDoneEvents(response.data);
+        });
+    }, []);  
 
+    function limitEvents() {
+
+        if (showAll) {
+            return doneEvents
+        }
         else {
-            api.get(`events?realizado=sim&limit=3`).then(response => {
-                setEventosRealizados(response.data);
-            });
+            return doneEvents.slice(0,3)
         }
-
-    }, [showMoreEvents]);
-
-
-    async function handleMoreEvents() {
-        setShowButton(false);
-        setShowMoreEvent(true);
     }
+
+    function handleClick() {
+
+        const y = window.scrollY
+
+        setShowAll(!showAll);
+        setTimeout(() => {
+            window.scrollTo(0, y)
+        }, 1)
+    }
+    
 
     return (
 
         <div class='container'>
             <Cabecalho />
-            <div class='titulo_eventos'>Nossos Eventos</div>
-            <p class='description'>
+            <span class='titles'>Nossos Eventos</span>
+            <p class='text'>
                 O Grupo Turing realiza eventos sendo alguns exclusivos<br />
                 à comunidade uspiana e outros abertos ao público externo.<br />
                 Veja alguns de nossos próximos eventos e eventos que já realizamos.<br />
             </p>
-            <div class='subtitulos'>Próximos Eventos</div>
-            <div class='ul_events'>
-                {eventosNaoRealizados.map(event => (
-                    <div class='li_events' key={ event.eventId }>
-                        <a target='_blank' href={event.link}>
-                            <img class='event-img' src={event.img}></img>
+            <span class='subtitle'>Próximos Eventos</span>            
+            {nextEvents[0] ? (
+                nextEvents.map(event => (
+                    <div class='next_events'>
+                        <a target='_blank' class='event' href={event.link} key={event.eventId} rel='noopener noreferrer'>
+                            <div class='img_ctr'>
+                                <img class='event_img next' src={event.img} />
+                            </div>
+                            <div class='name_ctr'>
+                                <p class='event_name'>{event.name}</p>
+                            </div>
                         </a>
                     </div>
+                ))
+            ) : (
+                <p class='text'>Não há eventos sendo preparados no momento.</p>
+            )}
+            <span class='subtitle'>Eventos Realizados</span>
+            <div class='done_events'>
+                {limitEvents().map(event => (
+                    <a target='_blank' class='event' href={event.link} key={event.eventId} rel='noopener noreferrer'>
+                        <div class='img_ctr'>
+                            <img class='event_img' src={event.img} />
+                        </div>
+                        <div class='name_ctr'>
+                            <p class='event_name'>{event.name}</p>
+                        </div>
+                    </a>
                 ))}
             </div>
-            { (eventosNaoRealizados[0] == null) && <p class='description'>Não há eventos sendo preparados no momento.</p>}
-            <div class='subtitulos'>Eventos Realizados</div>
-            <div class='ul_events'>
-                {eventosRealizados.map(event => (
-                    <div class='li_events' key={ event.eventId }>
-                        <a target='_blank' href={event.link}>
-                            <img class='event-img' src={event.img}></img>
-                        </a>
-                    </div>
-                ))}
-            </div>
-            <div class="wrapper">
-                {showButton && <button class='more_events_box' onClick={handleMoreEvents}>
-                    Mais Eventos
-                </button>}
+            <div class='toggler_ctr'>
+                <button class={`toggler ${showAll ? 'to_hide' : 'to_show'}`} onClick={handleClick} />
             </div>
             <Rodape />
         </div>
