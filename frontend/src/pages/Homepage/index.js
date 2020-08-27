3cac8eeccbf1d2ac5c69ecff92003a4ee8c7ec63
Gabriel Mossato <gvmossato@noreply.github.com>
2020-08-26 21:44:54 -0300

    Modificações na homepage e cabeçalho transparente
    Cabeçalho:
    * Passa a ser transparente na homepage, quando o scroll Y é 0.
    * Corrige a animação de mostrar e esconder a sidebar (mobile).
    
    Homepage:
    * Novas assets para cada tópico.
    * Novo "background azul" e redesign do topo dessa página.
    * Renomeia imagens.
    * Adiciona efeito "parallax" na versão mobile.
    * Remove assets desnecessárias.

--- frontend/src/pages/Homepage/index.js
+++ frontend/src/pages/Homepage/index.js
@@ -1,121 +1,87 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from 'react-dom';
-import {Link} from 'react-router-dom';
-import WindowSizeListener from 'react-window-size-listener'
-import {Background, Parallax} from 'react-parallax';
-import fundo from "../../assets/background.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Background, Parallax } from 'react-parallax';
+
 import './styles.css';
-import ialogo from "../../assets/imag-aprenda-ia.png";
-import projeto from "../../assets/apresentacao.png";
-import evento from "../../assets/eventos.png";
-import computador from "../../assets/foto-computador.png";
-import projetos from "../../assets/img-projetos.png";
+
+import pllx_network from '../../assets/background.png';
+import pllx_apresentacao from '../../assets/foto-apresentacao.png';
+import pllx_notebook from '../../assets/foto-notebook.png';
+
+import aprenda_ia from '../../assets/asset-aprenda-ia.png';
+import eventos from '../../assets/asset-eventos.png';
+import projetos from '../../assets/asset-projetos.png';
+
 import Cabecalho from '../../Tags/Cabecalho';
 import Rodape from '../../Tags/Rodape';
 
 
-export default function Homepage(){
+export default function Homepage() {
 
-
-    const [isVisible, setIsVisible] = useState(false);
-
-
-    useEffect(() => {
-        setIsVisible(window.innerWidth > 800);
-    },[window.innerWidth]);
-
-
-
-
-    return(
+    return(        
         <div class='homepage_container'>
             <Cabecalho />
-
-            <WindowSizeListener onResize={() => setIsVisible(window.innerWidth > 800)}>
-            </WindowSizeListener>
-                {isVisible && <Parallax style={{marginTop: "7vw"}}
-                strength={250}>
-                    <div style={{height: '60vw', width: '100%'}}>
-                        <div class="caixa-superior">
-                            <h1 class="texto-superior">
-                            Conheça o Grupo Turing: uma extensão universitária da Universidade de São Paulo (USP) que 
-                            acredita em gerar impacto disseminando, aplicando e estudando Inteligência Artificial
-                            </h1>
-                        </div>
-                        <br />
-                        <Link to="/AboutUs">
-                            <button class="saiba-mais" href="#">Saiba mais</button>
-                        </Link>
-                    </div>
-                    <Background>
-                        <img class='parallax_img' src={fundo} alt="fundo azul" />
-                    </Background>
-                </Parallax>}
-
-                {!isVisible && 
-                <div class='imagem-mobile'>
-                    <div class="caixa-superior">
-                        <h1 class="texto-superior">
-                        Conheça o Grupo Turing: uma extensão universitária da Universidade de São Paulo (USP) que 
-                        acredita em gerar impacto disseminando, aplicando e estudando Inteligência Artificial
+            <Parallax strength={250}>
+                <div class='intro'>
+                    <div class='caixa_superior'>
+                        <h1 class='texto_superior'>
+                            Conheça o Grupo Turing: uma extensão universitária da Universidade de São Paulo 
+                            (USP) que acredita em gerar impacto disseminando, aplicando e estudando Inteligência 
+                            Artificial
                         </h1>
                     </div>
-                    <Link to="/AboutUs">
-                        <button class="saiba-mais" href="#">Saiba mais</button>
+                    <Link to='/AboutUs'>
+                        <button class='saiba_mais' href='#'>Saiba mais</button>
                     </Link>
-                </div>}
+                </div>
+                <Background>
+                    <img class='parallax_superior' src={pllx_network} alt='technology_network' />
+                </Background>
+            </Parallax>
             <div class='bloco_info'>
-                <img class="img" src={ialogo} alt="logo-ia" />
-                <div class='textos'>
-                    <h1 class="titulo">Aprenda IA</h1>
-                    <h2 class="descricao">
-                    Estude Inteligência Artificial e suas ramificações com conteúdos produzidos por nós. Temos desde 
-                    vídeos de Workshops e palestras, até nossos inúmeros posts no Medium, com códigos presentes no 
-                    nosso GitHub.
+                <img class='img' src={aprenda_ia} alt='asset_aprenda_ia' />
+                <div class='caixa_textos'>
+                    <h1 class='titulos'>APRENDA IA</h1>
+                    <h2 class='descricao'>
+                        Estude Inteligência Artificial e suas ramificações com conteúdos produzidos por nós. 
+                        Temos desde vídeos de Workshops e palestras, até nossos inúmeros posts no Medium, com 
+                        códigos presentes no nosso GitHub.
                     </h2>
                 </div>
             </div>
-                {isVisible && <Parallax 
-                strength={250}>
-                    <div style={{height: '45vw', width: '100%'}}/>
-                    <Background  >
-                        <img class='parallax_img' src={projeto} alt="projeto"></img>
-                    </Background>
-                </Parallax>}
-                {!isVisible && <img class='parallax_img' src={projeto} alt="projeto"></img>}
-            <div class='bloco_info'>
-                <img class="img" src={evento} alt="evento" />
-                <div class='textos'>
-                    <h1 class="titulo">Eventos</h1>
-                    <h2 class="descricao">
-                    Descubra as aplicações em vida real que realizamos a partir de nossos estudos e qual o impacto que 
-                    IA pode ter no mundo!
+            <Parallax strength={250}>
+                <div class='parallax_window'/>
+                <Background>
+                    <img class='parallax_img' src={pllx_apresentacao} alt='apresentação' />
+                </Background>
+            </Parallax>                
+            <div class='bloco_info invert'>
+                <img class='img' src={eventos} alt='asset_eventos' />
+                <div class='caixa_textos'>
+                    <h1 class='titulos'>EVENTOS</h1>
+                    <h2 class='descricao'>
+                        Fique por dentro dos próximos eventos do grupo, que visam disseminar conteúdos 
+                        relacionados a IA na comunidade paulistana e brasileira.
                     </h2>
                 </div>
             </div>
-            <div>
-                {isVisible && <Parallax 
-                    strength={250}
-                >
-                    <div style={{height: '45vw', width: '100%'}}/>
-                    <Background>
-                    <img class='parallax_img' src={computador} alt="foto computador"></img>
-                    </Background>
-                </Parallax>}
-                {!isVisible && <img class='parallax_img' src={computador} alt="foto computador"></img>}
-            </div>
-            <div class="bloco_info">
-                <img class="img" src={projetos} alt="projetos" />
-                <div class="textos">
-                    <h1 class="titulo">Projetos</h1>
-                    <h2 class="descricao">
-                        Fique por dentro dos próximos eventos do grupo, que visam disseminar conteúdos relacionados a IA na 
-                        comunidade paulistana e brasileira.
+            <Parallax strength={250}>
+                <div class='parallax_window'/>
+                <Background>
+                    <img class='parallax_img' src={pllx_notebook} alt='notebook' />
+                </Background>
+            </Parallax>
+            <div class='bloco_info'>
+                <img class='img' src={projetos} alt='asset_projetos' />
+                <div class='caixa_textos'>
+                    <h1 class='titulos'>PROJETOS</h1>
+                    <h2 class='descricao'>
+                        Descubra as aplicações em vida real que realizamos a partir de nossos estudos e 
+                        qual o impacto que IA pode ter no mundo!
                     </h2>
                 </div>
             </div>
             <Rodape />
-        </div>
-            
+        </div>            
     );
 }
