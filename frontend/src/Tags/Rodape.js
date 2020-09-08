ec3020c01620fd57c8074d9deff8bbba10b34112
Gabriel Mossato <41247052+gvmossato@users.noreply.github.com>
2020-09-08 12:25:33 -0300

    Rodapé: alinhamento e melhorias (#109)
    * Corrige alinhamento do texto de endereço na versão mobile
    
    * Maior feedback ao usuário no hover do texto de endereço
    
    * Logo do rodapé e cabeçalho com cores idênticas
    
    * Substitui ícone de localização (remove asset desnecessária)
    
    Co-authored-by: Gabriel Mossato <gvmossato@noreply.github.com>

--- frontend/src/Tags/Rodape.js
+++ frontend/src/Tags/Rodape.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from 'react-dom';
-import {Link} from "react-router-dom"
-import logo2 from '../assets/logo2.png';
+import React from 'react';
+import { Link } from 'react-router-dom'
 
+import logo from '../assets/logo2.png';
 import instagram from '../assets/instagram.png';
 import facebook from '../assets/facebook.png';
 import youtube from '../assets/youtube.png';
@@ -10,28 +9,49 @@ import medium from '../assets/medium.png';
 import github from '../assets/github.png';
 import linkedin from '../assets/linkedin.png';
 
-import location from '../assets/add_location_24px.svg'
+import { TiLocation } from 'react-icons/ti'
+
 
 function Rodape() {
     return (
-        <div class= "rodape">
-            <Link to='/'><img class = "rodape_logo2" src={logo2} alt="Grupo Turing" /></Link>
-            <div class='rodape_social'>
-                    <p class='rodape_social_box'>Siga-nos nas redes sociais</p>
-                <br/>
-                <a target='_blank'href='https://www.instagram.com/grupoturing.usp/'><img src={instagram} class="link_rodape"/></a>
-                <a target='_blank'href='https://pt-br.facebook.com/grupoturing.usp/'><img src={facebook} class="link_rodape"/></a>
-                <a target='_blank'href='https://www.youtube.com/channel/UCIZSLh8_ST5np3k60qWOiAA'><img src={youtube} class="link_rodape"/></a>
-                <a target='_blank'href='https://medium.com/turing-talks'><img src={medium} class="link_rodape"/></a>
-                <a target='_blank'href='https://github.com/GrupoTuring'><img src={github} class="link_rodape"/></a>
-                <a target='_blank'href='https://www.linkedin.com/company/grupo-turing/?originalSubdomain=br'><img src={linkedin} class="link_rodape"/></a>
+        <div class='rodape'>
+            <div>
+                <Link to='/'>
+                    <img class='rodape_logo' src={logo} alt='grupo_turing' />
+                </Link>
             </div>
-            <div class='location'>
-                <a target='_blank' class='location'href='https://goo.gl/maps/Cw8RH7vbBo6DiuZTA'><p><img src={location} />InovaUSP</p>
-                <p class='location_text'>Av. Prof. Lúcio Martins Rodrigues, 370<br/>
-                Butantã <br/>
-                São Paulo - SP,<br />
-                05508-020</p></a>
+            <div class='social_ctr'>
+                <p class='social_title'>Siga-nos nas redes sociais</p>
+                <div class='social_grid'>
+                    <a target='_blank' href='https://www.instagram.com/grupoturing.usp/' rel='noopener noreferrer'>
+                        <img src={instagram} class='social_circle' alt='instagram' />
+                    </a>
+                    <a target='_blank' href='https://pt-br.facebook.com/grupoturing.usp/' rel='noopener noreferrer'>
+                        <img src={facebook} class='social_circle' alt='facebook' />
+                    </a>
+                    <a target='_blank' href='https://www.youtube.com/channel/UCIZSLh8_ST5np3k60qWOiAA' rel='noopener noreferrer'>
+                        <img src={youtube} class='social_circle' alt='youtube' />
+                    </a>
+                    <a target='_blank' href='https://medium.com/turing-talks' rel='noopener noreferrer'>
+                        <img src={medium} class='social_circle' alt='medium' />
+                    </a>
+                    <a target='_blank' href='https://github.com/GrupoTuring' rel='noopener noreferrer'>
+                        <img src={github} class='social_circle' alt='github' />
+                    </a>
+                    <a target='_blank' href='https://www.linkedin.com/company/grupo-turing/?originalSubdomain=br' rel='noopener noreferrer'>
+                        <img src={linkedin} class='social_circle' alt='linkedin' />
+                    </a>
+                </div>
+            </div>
+            <div class='location_ctr'>
+                <a target='_blank' class='location_link' href='https://goo.gl/maps/Cw8RH7vbBo6DiuZTA' rel='noopener noreferrer'>
+                    <TiLocation class='location_icon' />
+                    <p class='location_title'>InovaUSP</p>
+                </a>
+                    <p class='location_text'>
+                        Av. Prof. Lúcio Martins Rodrigues, 370<br />
+                        Butantã - São Paulo, SP
+                    </p>
             </div>
         </div>
     );
