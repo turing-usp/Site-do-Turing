833623d9f4602b9867954c31696fd42df33a407b
Lucas Leme <lucaslssantos99@gmail.com>
2020-10-12 15:14:59 -0300

    Fix ga api key

--- frontend/src/services/reactGA.js
+++ frontend/src/services/reactGA.js
@@ -2,7 +2,7 @@ import ReactGA from "react-ga";
 
 export class reactGoogleAnalytics {
   constructor() {
-    ReactGA.initialize("GA_API_KEY");
+    ReactGA.initialize(GA_API_KEY);
   }
 
   pageView(pageName) {
